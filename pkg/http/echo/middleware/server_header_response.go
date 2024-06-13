8433c21eb00841b66db96bbe762f7a8180bc8e13
mohamedmostafa <mohamed.mostafa@leannode.com>
2024-06-13 22:38:19 +0300

    allow ExposeHeaders

--- pkg/http/echo/middleware/server_header_response.go
+++ pkg/http/echo/middleware/server_header_response.go
@@ -5,10 +5,11 @@ import (
 	"os"
 )
 
+const HEADER_NAME = "STORAGE-URL"
+const HEADER_NAME_ENV = "STORAGE_URL"
+
 func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		HEADER_NAME := "STORAGE-URL"
-		HEADER_NAME_ENV := "STORAGE_URL"
 		browseUrl := os.Getenv(HEADER_NAME_ENV)
 		c.Response().Header().Set(HEADER_NAME, browseUrl)
 		return next(c)
