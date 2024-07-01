f21c7baa15f730475c8aebd3603381f1228c9d2f
AhmedHassanElbhai <ahmedhassanhassan707@gmail.com>
2024-07-02 00:48:34 +0300

    fix unique index

--- internal/module/admin/responses/role/find_role_response.go
+++ internal/module/admin/responses/role/find_role_response.go
@@ -14,6 +14,7 @@ type FindRoleResponse struct {
 	ID          primitive.ObjectID `json:"id"`
 	Name        Name               `json:"name"`
 	Permissions []string           `json:"permissions"`
+	CanDelete   bool               `json:"can_delete"`
 	CreatedAt   time.Time          `json:"created_at"`
 	UpdatedAt   time.Time          `json:"updated_at"`
 }
