29c5f198c470d79607788b2ee1c1141c311c1c6a
AhmedHassanElbhai <ahmedhassanhassan707@gmail.com>
2024-07-08 03:19:04 +0300

    fix pointer nil

--- internal/module/menu/policies/MenuGroupPolicy.go
+++ internal/module/menu/policies/MenuGroupPolicy.go
@@ -9,14 +9,14 @@ import (
 type MenuGroupPolicy struct {
 }
 
-func (p MenuGroupPolicy) Before(menuGroup domain.MenuGroup, ctx context.Context) bool {
+func (p MenuGroupPolicy) Before(menuGroup *domain.MenuGroup, ctx context.Context) bool {
 	if ctx.Value("causer-type") == "admin" {
 		return true
 	}
 	return false
 }
 
-func (p MenuGroupPolicy) Check(menuGroup domain.MenuGroup, ctx context.Context) bool {
+func (p MenuGroupPolicy) Check(menuGroup *domain.MenuGroup, ctx context.Context) bool {
 	if p.Before(menuGroup, ctx) {
 		return true
 	}
@@ -26,14 +26,14 @@ func (p MenuGroupPolicy) Check(menuGroup domain.MenuGroup, ctx context.Context) 
 	return false
 }
 
-func (p MenuGroupPolicy) Find(menuGroup domain.MenuGroup, ctx context.Context) bool {
+func (p MenuGroupPolicy) Find(menuGroup *domain.MenuGroup, ctx context.Context) bool {
 	return p.Check(menuGroup, ctx)
 }
 
-func (p MenuGroupPolicy) Update(menuGroup domain.MenuGroup, ctx context.Context) bool {
+func (p MenuGroupPolicy) Update(menuGroup *domain.MenuGroup, ctx context.Context) bool {
 	return p.Check(menuGroup, ctx)
 }
 
-func (p MenuGroupPolicy) Delete(menuGroup domain.MenuGroup, ctx context.Context) bool {
+func (p MenuGroupPolicy) Delete(menuGroup *domain.MenuGroup, ctx context.Context) bool {
 	return p.Check(menuGroup, ctx)
 }
